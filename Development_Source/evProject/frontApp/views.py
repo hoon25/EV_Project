bc8669d6dea2d12103e579000551ff7cdffe18b6
im-Lily <dmsruddlvhs2013@gmail.com>
2021-02-15 11:49:53 +0900

    Fix geo_2.html and models.py to marker and infowindow

--- Development_Source/evProject/frontApp/views.py
+++ Development_Source/evProject/frontApp/views.py
@@ -43,20 +43,20 @@ from django.http import HttpResponse
 def mygps(request) :
     print('request mygps - ')
     return render(request,'geo_2.html')
-    # return render(request, 'naverMapApiEx2.html')
+
 
 def evSearch(request) :
     if request.method == 'POST':
-        type = request.POST['type']
-        distance = request.POST['distance']
-        # stations = EvStation.objects.get(statnm=type)
+        user_lat = request.POST['lat']
+        user_lng = request.POST['lng']
     try:
         cursor = connection.cursor()
-        strSql = 'SELECT statNm,addr,lat,lng, \
-                    (6371*acos(cos(radians(37.5766831))*cos(radians(lat))*cos(radians(lng)-radians(126.8978620))+sin(radians(37.5766831))*sin(radians(lat))))AS distance \
-                    FROM ev_station \
+        strSql = "SELECT statNm,addr,lat,lng,useTime,stat,\
+                    (6371*acos(cos(radians("+user_lat+"))*cos(radians(lat))*cos(radians(lng)-radians("+user_lng+"))+sin(radians("+user_lat+"))*sin(radians(lat))))AS distance \
+                    FROM ev_station E \
+                    JOIN ev_station_status S ON(E.evsn = S.evsn) \
                     HAVING distance <= 2 \
-                    ORDER BY distance'
+                    ORDER BY distance"
 
         result = cursor.execute(strSql)
         stations = cursor.fetchall()
@@ -72,7 +72,9 @@ def evSearch(request) :
                    'addr': station[1],
                    'lat': station[2],
                    'lng': station[3],
-                   'distance': station[4]}
+                   'useTime' : station[4],
+                   'stat' : station[5],
+                   'distance': station[6]}
             list.append(row)
             cnt = cnt + 1
             if cnt == 5 :
@@ -86,28 +88,7 @@ def evSearch(request) :
 
 
 
-# def evSearch(request) :
-#     if request.method == 'POST' :
-#         lat = request.POST['lat'],
-#         lng = request.POST['lng']
-#         print('request evSearch -- ' , lat, lng)
-#         # stations = EvStation.objects.get(statnm=type)
-#         stations01 = EvStation.objects.filter(lat=lat)
-#         stations02 = EvStation.objects.filter(lng=lng)
-#         stations = [stations01, stations02]
-#         print('stations', stations)
-#         list = []
-#         cnt = 0
-#         for station in stations:
-#             list.append({
-#                 'statnm': station.statnm, 'addr': station.addr, 'lat': station.lat, 'lng': station.lng
-#             })
-#             cnt = cnt + 1
-#             if cnt == 5:
-#                 break
-#         for a in list:
-#             print("check - ", a)
-#         return JsonResponse(list, safe=False)
+
 
 
 def station(request):
@@ -137,46 +118,6 @@ def stationSearch(request):
         print("check - ", a)
     return JsonResponse(list, safe = False)
 
-
-
-
-# def evSearch(request):
-#     try:
-#         lat = request.POST['lat']
-#         lng = request.POST['lng']
-#         position = (lat, lng)
-#         condition = (
-#             Q(latitude__range  = (lat - 0.01, lat + 0.01)) | Q(longitude__range = (lng - 0.015, lng + 0.015))
-#         )
-#
-#         convenience_infos = (
-#             EvStation.objects.filter(condition)
-#         )
-#         near_convenience_infos = [info for info in convenience_infos
-#                                   if haversine(position, (info.latitude, info.longitude)) <= 2]
-#
-# return JsonResponse(near_convenience_infos, safe = False)
-
-
-
-
-# def latLng(request):
-#     if request.method == 'POST' :
-#         position = EvStation.objects.all()
-#         position = request.POST['position']
-#         print('request position - ', position)
-#         list = []
-#         cnt = 0
-#         for latlng in position :
-#             EvStation.lat = latlng.lat
-#             EvStation.lng = latlng.lng
-#             list.append({'lat' : latlng.lat,
-#                          'lng' : latlng.lng})
-#             cnt += 1
-#             if cnt == 5 :
-#                 break
-#         context = {'position' : list}
-#         return JsonResponse(list, safe=False)
 
 
 
